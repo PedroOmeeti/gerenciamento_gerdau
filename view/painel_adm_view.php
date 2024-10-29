6ec7eeea67979133ea05dc9760508634951a9d4e
Pedro Henrique <124195001+PedroOmeeti@users.noreply.github.com>
2024-10-29 17:04:37 -0300

    Navbar parcialmente finalizado
    Alterando o estilo do NavBar

--- view/painel_adm_view.php
+++ view/painel_adm_view.php
@@ -15,6 +15,14 @@
   require_once ('./components/Navbar.php');
   ?>
 
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="text-center pt-5">Painel Administrativo</h1>
+      </div>
+    </div>
+  </div>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
